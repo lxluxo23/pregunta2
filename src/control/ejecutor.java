bb7490cb421630781d94cbea726a68f7fae4894c
luis <luis@DESKTOP-5NEGP2Q>
2019-05-14 16:13:04 -0400

    modificcion sin menu

--- src/control/ejecutor.java
+++ src/control/ejecutor.java
@@ -6,9 +6,41 @@ public class ejecutor {
         
         
         MiLista l1 = new MiLista();
+        //l1.menu();
         
-        l1.insertar("wea");
-        l1.insertar("dos");
-        l1.InsertarEnPosicion();
+        
+        //********************************
+        l1.insertar("luis");
+        l1.insertar("cespedes");
+        //********************************
+        
+        
+        //********************************
+        
+                l1.InsertarEnPosicion(1,"universidad");
+        
+        //*******************************
+        
+                l1.ConsultarEnPosicion(1);
+                
+        //*******************************
+        
+        System.out.println("\n");
+        
+        //*******************************
+        
+                l1.EliminarElemento(0);
+        
+        //*******************************
+        
+        System.out.println("\n");
+        
+        //*******************************
+        
+            l1.modificar(0,"catolica");
+        
+        //*******************************
+        
+        
     }
 }
